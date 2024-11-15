6adf9265a72a1e8d9efa325713f85dffe08d6f26
Sujith S <sujithsureshkumar081@gmail.com>
2024-11-15 12:39:21 +0530

    unused import removed

--- lib/logic/logic.dart
+++ lib/logic/logic.dart
@@ -3,17 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
-import '../constant/constant.dart';
-
-
-
 class Logic extends GetxController {
-
   // ignore: unnecessary_cast
   final Rx<File?> pdf = (null as File?).obs;
-  RxList<File?> fileList=<File?>[].obs;
-  RxList<Widget?> widgetList=<Widget?>[].obs;
-  Rx<ImageSource> source=ImageSource.camera.obs;
-
-
+  RxList<File?> fileList = <File?>[].obs;
+  RxList<Widget?> widgetList = <Widget?>[].obs;
+  Rx<ImageSource> source = ImageSource.camera.obs;
 }
